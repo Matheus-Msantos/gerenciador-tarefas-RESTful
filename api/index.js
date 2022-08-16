77d42110dadbd9b5bd3095f083a78019f0b6b34b
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-08-16 18:58:11 -0300

    importando recursos

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const bodyRarser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3001;
+
+//Midleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//to-do Criar URLs da API
+//Requisições (get, post, put, delete)
+
 
 
 app.listen(port, () => console.log(`Servido online in port: ${port}`));
