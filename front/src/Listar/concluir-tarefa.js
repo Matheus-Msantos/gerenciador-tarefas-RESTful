76b1914afe0afc0ce5de41c0bd13ddcde3cddd7b
Matheus-Msantos <matheusmelosantos10@gmail.com>
2022-10-17 19:06:52 -0300

    Add API concluir tarefa

--- front/src/Listar/concluir-tarefa.js
+++ front/src/Listar/concluir-tarefa.js
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 function ConcluirTarefa(props) {
 
-  const [modal, setModal] = useState(false)
+  const API_CONCLUIR_TAREFA = 'http://localhost:3001/gerenciador-tarefas/:id/concluir';
+
+  const [modal, setModal] = useState(false);
+  const [modalErr, setModalErr] = useState(false);
+
+  const mostrarModalErr = (item) => {
+    setModalErr(item);
+  }
 
   const abrirModal = () => {
     setModal(true);
@@ -13,18 +21,15 @@ function ConcluirTarefa(props) {
     setModal(false);
   }
 
-  const conclurTarefa = () => {
-    const tarefasDb = localStorage['tarefas'];
-    let tarefas = tarefasDb ? JSON.parse(tarefasDb) : [];
-    tarefas = tarefas.map(tarefa => {
-      if (tarefa.id === props.tarefas.id) {
-        tarefa.concluida = true;
-      }
-      return tarefa;
-    });
-    localStorage['tarefas'] = JSON.stringify(tarefas);
-    setModal(false);
-    props.recarregarTarefas(true);
+  const conclurTarefa = async () => {
+    try {
+      await axios.put(API_CONCLUIR_TAREFA.replace(':id', props.tarefas.id));
+      setModal(false);
+      props.recarregarTarefas(true);
+
+    } catch (err) {
+      setModalErr(true);
+    }
   }
 
   return (
@@ -46,6 +51,19 @@ function ConcluirTarefa(props) {
         <div className="g-concluir-modal-footer">
           <button className="g-concluir-modal-btn-close" data-testid="modal-btn-fechar" onClick={fecharModal}>Não</button>
           <button className="g-concluir-modal-btn-success" onClick={() => conclurTarefa()} data-testid="modal-btn-concluir">Sim</button>
+        </div>
+      </div>
+
+      <div className={modalErr ? `g-cadastrar-modal-overley--active` : `g-cadastrar-modal-overley`} onClick={() => mostrarModalErr(false)}></div>
+      <div className={modalErr ? `g-cadastrar-modal-container--active` : 'g-cadastrar-modal-container'} data-testid="modal">
+        <div className="g-cadastrar-modal-header">
+          <p>Erro</p>
+        </div>
+        <div className="g-cadastrar-modal-body">
+          <p>Erro ao concluir tarefa, tente novamente mais tarde!</p>
+        </div>
+        <div className="g-cadastrar-modal-footer">
+          <button className="g-cadastrar-modal-btn--new" onClick={() => mostrarModalErr(false)}>Fechar</button>
         </div>
       </div>
     </>
